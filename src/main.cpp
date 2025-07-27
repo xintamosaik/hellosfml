e9a24fa829cb0603b746078a7755a9e1303ff624
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-07-27 13:01:53 +0200

    Add Keyboard events

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 int main() {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
@@ -14,8 +14,26 @@ int main() {
                 window.close();
             } else if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>()) {
                 if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
-                    window.close();
-            }
+                {
+                    std::cout << "the escape key was pressed" << std::endl;
+                    std::cout << "scancode: " << static_cast<int>(keyPressed->scancode) << std::endl;
+                    std::cout << "code: " << static_cast<int>(keyPressed->code) << std::endl;
+                    std::cout << "control: " << keyPressed->control << std::endl;
+                    std::cout << "alt: " << keyPressed->alt << std::endl;
+                    std::cout << "shift: " << keyPressed->shift << std::endl;
+                    std::cout << "system: " << keyPressed->system << std::endl;
+                    std::cout << "description: " << sf::Keyboard::getDescription(keyPressed->scancode).toAnsiString() << std::endl;
+                    std::cout << "localize: " << static_cast<int>(sf::Keyboard::localize(keyPressed->scancode)) << std::endl;
+                    std::cout << "delocalize: " << static_cast<int>(sf::Keyboard::delocalize(keyPressed->code)) << std::endl;
+                }
+                // by key code
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+                    std::cout << "the left mouse button was pressed" << std::endl;
+                }
+                // by scan code
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Right)) {
+                    std::cout << "right pressed" << std::endl;
+                }}
         }
 
         window.clear();
